444eb8818cc9d5b27403d91be6acf803be418612
sharath25187 <sharath25187@gmail.com>
2025-07-27 13:49:55 +0530

    Fix: Use dynamic SSL config for Render vs local

--- db.js
+++ db.js
@@ -2,18 +2,24 @@ const mysql = require('mysql2');
 const fs = require('fs');
 require('dotenv').config();
 
+// Detect if running on Render
+const isRender = process.env.RENDER === 'true';
+
+// Set SSL config only if on Render
+const sslConfig = isRender
+  ? {
+      ca: fs.readFileSync('/etc/secrets/ca.pem')
+    }
+  : undefined; // No SSL for local
 
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    port:process.env.DB_PORT,
-    ssl: {
-      ca: fs.readFileSync('./ca.pem') 
-   }
-  });
-  
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: sslConfig
+});
 
 db.connect(err => {
   if (err) {
@@ -24,4 +30,7 @@ db.connect(err => {
 });
 
 module.exports = db;
+
+
+
 

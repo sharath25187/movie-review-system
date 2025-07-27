348b396cc04c4f46fcd68cb82cf74bab071ccd61
sharath25187 <sharath25187@gmail.com>
2025-07-27 12:22:26 +0530

    Deploy-ready code for Render

---
+++ db.js
@@ -0,0 +1,27 @@
+const mysql = require('mysql2');
+const fs = require('fs');
+require('dotenv').config();
+
+
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    port:process.env.DB_PORT,
+    ssl: {
+      ca: fs.readFileSync('./ca.pem') 
+   }
+  });
+  
+
+db.connect(err => {
+  if (err) {
+    console.error('Connection error:', err);
+  } else {
+    console.log('Connected to Aiven MySQL!');
+  }
+});
+
+module.exports = db;
+
